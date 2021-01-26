a3deec4143d172321eff8343bbb0c1b9894b91a5
cgarchbold <cgarchbold@gmail.com>
2021-01-26 13:03:25 -0500

    Moving from other Repo

---
+++ dataset.py
@@ -0,0 +1,154 @@
+import os
+import torch
+import geopandas as gp
+import glob
+import pandas as pd
+from torch.utils.data import Dataset
+from PIL import Image
+import rasterio
+import numpy as np
+
+# This dataset class is for full inspection of the dataset, used for debugging
+class HennepinDatasetFull(Dataset):
+    def __init__(self, csv_path, shapefile_path, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.bbox_frame = pd.read_csv(csv_path)
+        self.shp_path = shapefile_path
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.bbox_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        #Bounding Box 
+        row = self.bbox_frame.iloc[idx]
+        row_bbox = (row['lat_min'], row['lon_min'],row['lat_max'], row['lon_max'])
+
+        #Polygons
+        gdf = gp.read_file(self.shp_path, bbox = row_bbox)
+        #geometry = gdf['geometry']
+
+        #Value
+        value = gdf['TOTAL_MV1']
+
+        #Image Bounding Boxw
+        image_bbox = (row['lat_min'], row['lat_max'],row['lon_min'], row['lon_max'])
+        
+        #Image
+        img_path = os.path.join(self.root_dir, str(int(row['lat_mid'])), str(int(row['lon_mid'])))
+        pthList = sorted(glob.glob(img_path + '/*.tif'))
+
+        #print(pthList)
+
+        #print(img_path)
+        #image = Image.open(pthList[0])
+        raster = rasterio.open(pthList[0])
+        array = raster.read()
+
+        # Parcel Value Label
+        try:
+            label_raster = rasterio.open(pthList[4])
+            pvalue_label = label_raster.read()
+            pvalue_label = np.flip(pvalue_label, 1)
+        except IndexError:
+            pvalue_label = 0
+
+        # Parcel Mask Label
+        try:
+            label_raster = rasterio.open(pthList[3])
+            pmask_label = label_raster.read()
+            pmask_label = np.flip(pmask_label, 1)
+        except IndexError:
+            pmask_label = 0
+
+        # Parcel Boundary Label
+        try:
+            label_raster = rasterio.open(pthList[2])
+            pboundary_label = label_raster.read()
+            pboundary_label = np.flip(pboundary_label, 1)
+        except IndexError:
+            pboundary_label = 0
+
+        # Building Label
+        try:
+            label_raster = rasterio.open(pthList[1])
+            building_label = label_raster.read()
+            building_label = np.flip(building_label, 1)
+        except IndexError:
+            building_label = 0
+        
+        #Sample
+        sample = {'image': array,'pvalue_label': pvalue_label,'pboundary_label':pboundary_label,'pmask_label': pmask_label,'building_label': building_label, 'raster': raster, 'bbox': row_bbox, 'img_bbox': image_bbox, 'geometry': gdf, 'value': value}
+
+        return sample
+
+class HennepinDataset(Dataset):
+    def __init__(self, csv_path, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.bbox_frame = pd.read_csv(csv_path)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.bbox_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        #Bounding Box 
+        row = self.bbox_frame.iloc[idx]
+        row_bbox = (row['lat_min'], row['lon_min'],row['lat_max'], row['lon_max'])
+
+        #Path
+        img_path = os.path.join(self.root_dir, str(int(row['lat_mid'])), str(int(row['lon_mid'])))
+        pthList = sorted(glob.glob(img_path + '/*.tif'))
+
+        #Image
+        raster = rasterio.open(pthList[0])
+        array = raster.read()
+
+        # Parcel Value Label
+        try:
+            label_raster = rasterio.open(pthList[3])
+            pvalue_label = label_raster.read()
+            pvalue_label = np.flip(pvalue_label, 1)
+        except IndexError:
+            pvalue_label = 0
+
+        # Parcel Mask Label
+        try:
+            label_raster = rasterio.open(pthList[2])
+            pmask_label = label_raster.read()
+            pmask_label = np.flip(pmask_label, 1)
+        except IndexError:
+            pmask_label = 0
+
+        # Building Label
+        try:
+            label_raster = rasterio.open(pthList[1])
+            building_label = label_raster.read()
+            building_label = np.flip(building_label, 1)
+        except IndexError:
+            building_label = 0
+
+        #Sample
+        sample = {'image': array,'pvalue_label': pvalue_label, 'pmask_label': pmask_label,'building_label':building_label,'bbox': row_bbox}
+
+        return sample
---
+++ satellite_gather.py
@@ -0,0 +1,108 @@
+import os
+import csv
+import time
+import glob
+import numpy as np
+import pandas as pd
+import itertools
+from tqdm import tqdm
+import urllib.request
+from multiprocessing import Pool
+
+import utils
+import argparse
+'''
+Making a note here of the WMS server...
+
+Some useful links for debugging request:
+https://gis.hennepin.us/arcgis/services/Imagery/UTM_Aerial_2020/MapServer/WMSServer?request=GetCapabilities&service=WMS
+https://kygisserver.ky.gov/arcgis/rest/services/WGS84WM_Services/Ky_Imagery_2019_6IN_WGS84WM/MapServer
+'''
+parser = argparse.ArgumentParser(description='Download Image Set to folder')
+parser.add_argument('--path', type=str, help='Directory to download to.', default= './downloads/')
+parser.add_argument('--gsd', type=int, help='Ground Sample Distance', default=1)
+args = parser.parse_args()
+
+
+def download(item):
+  url = item[0]
+  fn = item[1]
+
+  if not os.path.exists(fn):
+    utils.ensure_dir(fn)
+    try:
+      urllib.request.urlretrieve(url, fn)
+    except:
+      print('error while retrieving {:}'.format(url))
+  else:
+    pass
+    #print('Skipped ', fn.split('/')[-1])
+
+
+if __name__ == "__main__":
+
+  #image size in meters
+  image_width = 302 * int(args.gsd)
+  delta_X = image_width / 2
+
+  #ESPG:26915 - meters
+  X_min_bound = 440000  
+  X_max_bound = 473000
+  Y_min_bound = 4974000
+  Y_max_bound = 4988000
+
+  #Mesh
+  X = np.arange( X_min_bound, X_max_bound, step = image_width )
+  Y = np.arange( Y_min_bound, Y_max_bound, step = image_width )
+  mesh = np.array(np.meshgrid(X,Y))
+
+  combinations = mesh.T.reshape(-1, 2)
+
+  locations_to_download = np.unique(combinations, axis=0)
+
+  #Make some crummy bounding boxes, TODO make these good shapes in meters
+  df = pd.DataFrame(
+      np.concatenate([locations_to_download + x for x in [-delta_X, 0, delta_X]],
+                     axis=1),
+      columns=('lat_min', 'lon_min', 'lat_mid', 'lon_mid', 'lat_max',
+               'lon_max'))
+
+  #naip_dir = '/u/eag-d1/scratch/ebird/naip/'
+
+  #print(df.head())
+
+  if __name__ == '__main__':
+    #utils.ensure_dir(naip_dir)
+
+    #template_url = 'http://kyraster.ky.gov/arcgis/services/ImageServices/Ky_NAIP_2014_1M/ImageServer/WMSServer?request=GetMap&service=WMS&layers=0&CRS=EPSG:4326&BBOX={:},{:},{:},{:}&width=302&height=302&format=image/tif'
+    template_url = 'https://gis.hennepin.us/arcgis/services/Imagery/UTM_Aerial_2020/MapServer/WMSServer?version=1.3.0&&service=WMS&request=GetMap&&styles=&layers=0&CRS=EPSG:26915&BBOX={:},{:},{:},{:}&width=302&height=302&format=image/tiff'
+
+    print('Preparing jobs')
+
+    jobs = []
+    for idx, row in tqdm(df.iterrows(), total=len(df)):
+      #if idx % 10000 == 0: print('%d / %d' % (idx, len(df)))
+
+      image_url = template_url.format(row['lat_min'], row['lon_min'],
+                                      row['lat_max'], row['lon_max'])
+                                      
+      print(image_url)
+      out_file = args.path + "{:}/{:}/{:}_{:}.tif".format(
+          int(row['lat_mid']), int(row['lon_mid']),
+          row['lat_mid'], row['lon_mid'])
+      jobs.append([image_url, out_file])
+
+  print('Started downloading')
+  start = time.time()
+  p = Pool(10)
+
+  for fn in tqdm(p.imap_unordered(download, jobs), total=len(jobs)):
+    pass
+
+
+  csv_outfile = args.path + 'hennepin_bbox_ver1.csv'
+
+  df.to_csv(csv_outfile)
+
+
+  end = time.time()
